8217114b672e9dd052013e90332ca3bdd120dbc5
Alistair Chambers <alicham6@gmail.com>
2026-04-27 22:59:04 -0500

    esp32 ble version

---
+++ esp32 version/main_esp.cpp
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <BleMouse.h>
+
+BleMouse bleMouse;
+
+unsigned long previousMicros = 0;
+const unsigned long interval = 1000000; // 1`,000,000 us = 1000 ms
+
+void setup() {
+  Serial.begin(115200);
+  bleMouse.begin();
+  // We can`'t click immediately; delay for setup
+  delay(2000); 
+  previousMicros = micros();
+  Serial.println("Setup complete! Waiting for Bluetooth connection...");
+}
+
+void loop() {
+  unsigned long currentMicros = micros();
+  
+  if (currentMicros - previousMicros >= interval) {
+    previousMicros += interval; 
+    
+    // Only click if a device is connected via Bluetooth`
+    if(bleMouse.isConnected()) {
+      Serial.println("Connected - Sending Forward Click!");
+      bleMouse.click(MOUSE_FORWARD);
+    } else {
+      Serial.println("Waiting for Bluetooth pairing...");
+    }
+  }
+}
